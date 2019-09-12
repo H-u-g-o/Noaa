094ed414cddb17a12b4c0d2b48602da849acf278
H <hros38@gmail.com>
2019-09-12 11:10:20 +0200

    first commit

---
+++ noaa.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import tarfile
+import gzip
+import re
+import os
+import datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tarfile
+import gzip
+
+class getDf:
+
+    def __init__(self, year, station):
+        self.year = str(year)
+        self.station = str(station)
+
+    def getYear(self):
+        with tarfile.open('./gsod_all_years/gsod_'+self.year+'.tar') as tar:
+            tar.extractall(path='./temp_'+self.year)
+
+    def getStation(self):
+        with gzip.open('./temp_'+self.year+'/'+self.station+'-'+self.year+'.op.gz','rb') as station_file:
+            for line in station_file:
+                station_df = pd.read_csv(station_file, delim_whitespace = True, header=None)
+        return station_df
+
+
+class Noaa:
+
+    def __init__(self, yearBegin, yearEnd, station):
+        self.yearBegin = int(yearBegin)
+        self.yearEnd = int(yearEnd)
+        self.station = str(station)
+
+    def getSeveralyear(self):
+        liste_df = []
+        for i in range (self.yearBegin, self.yearEnd+1):
+            n = getDf(i, self.station )
+            n.getYear()
+            liste_df.append(n.getStation())
+            print(liste_df)
+        all_data = pd.concat(liste_df, ignore_index=True)
+        columns = ['STN','WBAN','YEARMODA','TEMP','COUNT_1','DEWP','COUNT_2','SLP','COUNT_3','STP','COUNT_4','VISIB','COUNT_5','WDSP','COUNT_6','MXSPD','GUST','MAX','MIN','PRCP','SNDP','FRSHTT']
+        all_data.columns=columns
+        all_data.to_csv('testo.csv')
+
+
+n = Noaa(2018,2019,"071560-99999")
+n.getSeveralyear()
+
+
+
+#station_df.to_csv(self.year+'_'+self.station+'.csv')
